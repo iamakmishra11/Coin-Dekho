62021a86d20679acb95f91a95518ff9bc9ae224a
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-12-18 00:37:16 +0530

    update 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
+  /** @type {import('tailwindcss').Config} */
+  import daisyui from 'daisyui';
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ['dracula'],
-  },
-};
+  export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+    daisyui: {
+      themes: ['dracula'],
+    },
+  };
